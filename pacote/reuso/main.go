6929bd06cc2a3898e8b2f387cab84e39084de13c
barbara <barbara31fab@gmail.com>
2020-01-22 21:40:51 -0300

    usando lib

---
+++ pacote/reuso/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/barbara-oliveira/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(2))
+	fmt.Println(area.Rect(4, 7))
+}
---
+++ pacote/usandolib/usandolib.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/barbara-oliveira/goarea"
+)
+
+func main() {
+	area := goarea.Circ(4.0)
+	fmt.Println(area)
+}
